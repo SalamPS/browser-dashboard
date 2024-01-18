6f3fba6dca9f4ab74e6981cfd5409ca4b429fa38
LamP <senzawa.g@gmail.com>
2024-01-18 19:08:16 +0700

    chore(widget): Stylized

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,40 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react"
+
+const Widget = ({isEmpty, newBlock}) => {
+  return (
+    <div className={`block ${isEmpty ? 'empty' : ''}`}>
+      {isEmpty?
+      <div className="widget insert" onClick={newBlock}>
+        <span>+</span>
+      </div>
+      :
+      <div className="widget feat">
+        <div className="body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, delectus?</div>
+      </div>}
+    </div>
+  )
+}
+
 export default function App () {
+  const [Block, setBlock] = useState([
+    {},
+    {isEmpty: true},
+  ])
+
+  const newBlock = () => {
+    const copy = Block.slice();
+    if(copy.length == 6) copy.pop();
+    copy.splice(copy.length-2, 0, {});
+    setBlock(copy)
+  }
+  const removeBlock = () => {
+    const copy = Block.slice();
+    if(copy.length == 6) copy.pop();
+    copy.splice(copy.length-2, 0, {});
+    setBlock(copy)
+  }
+
   return (<>
     <main>
       <div className="container">
@@ -9,7 +45,10 @@ export default function App () {
         </div>
         <div className="blocks">
           <div className="box">
-            <div className="block"></div>
+            {Block.map((content, i) => (<Widget key={i} 
+              isEmpty={content.isEmpty}
+              newBlock={newBlock}
+            />))}
           </div>
           <div>By LamP</div>
         </div>
