fd09a0342e53ae995feb796f23198249a68305d3
LamP <senzawa.g@gmail.com>
2024-01-18 15:34:42 +0700

    chore(widget): Blocks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from "react"
-
 export default function App () {
   return (<>
-  
+    <main>
+      <div className="container">
+        <div className="todo">
+          <h1>
+            ToDo
+          </h1>
+        </div>
+        <div className="blocks">
+          <div className="box">
+            <div className="block"></div>
+          </div>
+          <div>By LamP</div>
+        </div>
+      </div>
+    </main>
   </>)
 }
