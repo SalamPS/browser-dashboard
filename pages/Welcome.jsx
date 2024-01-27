dd45e2f7b3a6becd39f90817fde9633a8b81e448
LamP <senzawa.g@gmail.com>
2024-01-28 05:02:38 +0700

    style(shortcut): Stylized with no Functional

--- pages/Welcome.jsx
+++ pages/Welcome.jsx
@@ -1,17 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
 import Time from "./Time";
 import Cookies from 'js-cookie';
 
-const Short = () => {
-  return (<>
-    <div className="short">
-
-    </div>
-  </>)
-}
-
 export default function Welcome ({savedConfig, setSavedConfig, storageKey, userConfig, setUserConfig, Valid, setValid}) {
   Cookies.set('token', 'eyJuYW1lIjoic2FsYW1wYXJhcnRhIiwiaG9zdCI6InNhbGFtcGFyYXJ0YSIsImFsZyI6IkhTMjU2In0.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.62St0g6EesPAu0JuqZyKGFEzZEJmp_C8PWlwD5U-d7Y', { expires: 7 });
-  
+
   return (<>
     <div className="welcome">
       <div className="inner">
@@ -21,7 +14,21 @@ export default function Welcome ({savedConfig, setSavedConfig, storageKey, userC
             <Time/>
           </div>
           <div className="shortcut">
-            
+            {!userConfig ? <></> :
+            userConfig.short.map((item) => (
+              <a href={item.url} className="short" key={item.id_short} target="_blank">
+                <img src={item.favicon} alt={item.name} /> <br />
+                <span>{item.name}</span>
+              </a>
+            ))}
+            {userConfig.short.length > 6 ? '' : 
+            <div className="short">
+              <div className="add">
+                <button>
+                  +
+                </button>
+              </div>
+            </div>}
           </div>
         </div>
       </div>
