186569b5217f0c68dde81b22996c904d285fb515
LamP <senzawa.g@gmail.com>
2024-01-29 15:00:17 +0700

    feat(push): Push Data

--- pages/Welcome.jsx
+++ pages/Welcome.jsx
@@ -1,12 +1,81 @@
 /* eslint-disable @next/next/no-img-element */
+import { useState } from "react";
 import Time from "./Time";
 import Cookies from 'js-cookie';
 
-export default function Welcome ({savedConfig, setSavedConfig, storageKey, userConfig, setUserConfig, Valid, setValid}) {
+export default function Welcome ({savedConfig, setSavedConfig, storageKey, userConfig, setUserConfig, Valid, setValid }) {
   Cookies.set('token', 'eyJuYW1lIjoic2FsYW1wYXJhcnRhIiwiaG9zdCI6InNhbGFtcGFyYXJ0YSIsImFsZyI6IkhTMjU2In0.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.62St0g6EesPAu0JuqZyKGFEzZEJmp_C8PWlwD5U-d7Y', { expires: 7 });
+  const [ToggleShortcut, setToggleShortcut] = useState(false)
+  const [formData, setFormData] = useState({
+    id_short: 0,
+    name: '',
+    url: ''
+  })
+  
+  // Setup Data for Shortcut
+  // 
+  const saveShort = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch('/api/default?dest=short', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+    } catch (error) {
+      console.error('Error during POST request:', error);
+    }
+    setToggleShortcut(false)
+
+    const temp = {...userConfig}
+    temp.short.push(formData)
+    setUserConfig(temp)
+    localStorage.setItem(storageKey, JSON.stringify(temp))
+  }
+
+  const handleChange = (name, value) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }))
+  }
 
   return (<>
     <div className="welcome">
+      <div className="addShort" style={ToggleShortcut ? {zIndex: 2, opacity: 1} : {zIndex: -1, opacity: 0}}>
+        <div className="input">
+          <form onSubmit={(e) => {saveShort(e)}}>
+            <h3>New Shortcut</h3>
+            <label>
+              <span>Name</span>
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={(e) => handleChange(e.target.name, e.target.value)}
+              />
+            </label>
+            <label>
+              <span>Link</span>
+              <input
+                type="text"
+                name="url"
+                value={formData.url}
+                onChange={(e) => handleChange(e.target.name, e.target.value)}
+              />
+            </label>
+            <div className="button">
+              <button id="cancel" title="Cancel" onClick={() => {setToggleShortcut(false)}}>Cancel</button>
+              <button id="submit" type="submit" title="Save new List">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
       <div className="inner">
         <div className="pad">
           <div className="head">
@@ -16,18 +85,17 @@ export default function Welcome ({savedConfig, setSavedConfig, storageKey, userC
           <div className="shortcut">
             {!userConfig ? <></> :
             userConfig.short.map((item) => (
-              <a href={item.url} className="short" key={item.id_short} target="_blank">
-                <img src={item.favicon} alt={item.name} /> <br />
+              <a href={item.url} className="short cut" key={item.id_short} target="_blank">
+                <img src={`https://logo.clearbit.com/${item.url}`} alt={item.name} /> <br />
                 <span>{item.name}</span>
               </a>
             ))}
             {userConfig.short.length > 6 ? '' : 
             <div className="short">
-              <div className="add">
-                <button>
-                  +
-                </button>
-              </div>
+              <button onClick={() => {
+                handleChange('id_short', new Date().getTime() / 1000)
+                setToggleShortcut(true)
+              }}>+</button>
             </div>}
           </div>
         </div>
