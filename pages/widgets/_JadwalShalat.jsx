c74ac2e2f01accc161add0b805f1e64852ef5c6b
LamP <senzawa.g@gmail.com>
2024-02-11 11:34:04 +0700

    feat(fetch): Success form of listing

--- pages/widgets/_JadwalShalat.jsx
+++ pages/widgets/_JadwalShalat.jsx
@@ -1,10 +1,61 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
+import { useEffect, useState } from "react"
+
 export default function JadwalShalat ({remove, id}) {
+  const [City, setCity] = useState('1201')
+  const [Data, setData] = useState(
+    {
+      id:0,
+      lokasi:"",
+      daerah:"",
+      jadwal:{tanggal:"", imsak:"", subuh:"", terbit:"", dhuha:"", dzuhur:"", ashar:"", maghrib:"", isya:"", date:""}
+    }
+  )
+  
+  useEffect(() => {
+    if (City) {
+      const date = new Date(Date.now())
+      const req = `https://api.myquran.com/v2/sholat/jadwal/${City}/`
+      + (date.getFullYear()) + '-'
+      + (date.getMonth()+1) + '-'
+      + (date.getDate())
+
+      fetch(req)
+      .then(async res => {
+        const result = await res.json()
+        setData(result.data)
+      })
+      .catch(err => console.error('Error:', err))
+    }
+  }, [])
+
+  const Shalat = ({type}) => {
+    return (<div className={`shalat ${type}`}>
+      <span className="name">
+        {type.charAt(0).toUpperCase() + type.slice(1)}
+      </span>
+      <span className="time">
+        {Data.jadwal[type]}
+      </span>
+    </div>)
+  }
+
   return ( <>
     <h2>
       <span>Jadwal Shalat</span>
       <i className="bi bi-dash-circle-fill delete" onClick={() => {remove(id)}}></i>
     </h2>
+    {City ? 
+    <div className="view">
+      <Shalat type={'subuh'}/>
+      <Shalat type={'dzuhur'}/>
+      <Shalat type={'ashar'}/>
+      <Shalat type={'maghrib'}/>
+      <Shalat type={'isya'}/>
+    </div> 
+    : 
+    <div className="select">
+    </div>}
   </>)
 }
